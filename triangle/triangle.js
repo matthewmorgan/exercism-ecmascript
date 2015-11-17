710c78bcd4d7aacd79978b42e0b7b0e4654ec346
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-17 09:01:25 -0500

    refactored triangle

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,15 +1,11 @@
 let sides;
 const TRIANGLE_KINDS = {1: 'equilateral', 2: 'isosceles', 3: 'scalene'};
 
-const hasSize = () => {
-  if (sides.some(side => side === 0 )) throw new Error('Triangles must have size!');
+const throwOnIllegalSides = () => {
+  if (sides.some(side => side <= 0 )) throw new Error('Side lengths must be positive!');
 };
 
-const hasNoNegativeSides = () => {
-  if (sides.some(side => side < 0)) throw new Error('Negative sides are not allowed!');
-};
-
-const doesntViolateInequality = () => {
+const throwOnViolatesInequality = () => {
   if (sides[2] > (sides[0] + sides[1])) throw new Error('Sum of two sides must exceed third side!');
 };
 
@@ -21,9 +17,8 @@ export default class Triangle {
   }
 
   kind() {
-    hasSize();
-    hasNoNegativeSides();
-    doesntViolateInequality();
+    throwOnIllegalSides();
+    throwOnViolatesInequality();
     const uniqueSideCount = (new Set(sides)).size;
     return TRIANGLE_KINDS[uniqueSideCount];
   }
