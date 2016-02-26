97ab6e8a1ff07f16e4293251060bfef646303b7b
Matthew Morgan <matt@dulcetsoftware.com>
2016-02-26 06:27:59 -0500

    Refactor crypto-square

--- crypto-square/crypto-square.js
+++ crypto-square/crypto-square.js
@@ -1,43 +1,52 @@
-let squareSize;
+let normalizedSize;
 
-const getSegments = str => {
+export default class Crypto {
+
+  constructor(phrase) {
+    this.phrase = phrase;
+    this.squareSize = calculateSquareSize(this.phrase);
+    normalizedSize = calculateSquareSize(this.normalizePlaintext(this.phrase));
+    this.ciphertext = () => getCipherText(this.plaintextSegments(), this.squareSize);
+  }
+
+  size(){
+    return normalizedSize;
+  }
+
+  plaintextSegments(){
+    const normalized = this.normalizePlaintext(this.phrase);
+    return getSegments(normalized, normalizedSize);
+  }
+
+  normalizeCipherText(){
+    return getSegments(this.ciphertext, this.squareSize).join(' ');
+  }
+
+  normalizePlaintext(){
+    return this.phrase.toLowerCase().replace(/[\W]/g, '');
+  }
+
+}
+
+function calculateSquareSize(str){
+  return Math.ceil(Math.sqrt(str.length));
+}
+
+function getSegments(str, segmentSize) {
   const result = [];
   let source = [...str];
   while (source.length > 0) {
-    result.push(source.splice(0, squareSize).join(''));
+    result.push(source.splice(0, segmentSize).join(''));
   }
   return result;
 }
 
-const getCipherText = square => {
+function getCipherText(square, segmentSize){
   let cipherText = '';
-  for (let ii = 0; ii < squareSize; ii++) {
+  for (let ii = 0; ii < segmentSize; ii++) {
     for (let jj = 0; jj < square.length; jj++) {
       cipherText += square[jj][ii] || "";
     }
   }
   return cipherText;
-}
-
-const normalizePlaintext = str => str.toLowerCase().replace(/[^\w]/g, '');
-const getSquareSize = str => Math.ceil(Math.sqrt(str.length));
-
-export default class Crypto {
-  constructor(phrase) {
-    squareSize = 0;
-    return {
-      normalizePlaintext:  () => normalizePlaintext(phrase),
-      size:                () => getSquareSize(normalizePlaintext(phrase)),
-      plaintextSegments:   function () {
-        const normalized = normalizePlaintext(phrase);
-        squareSize = this.size(normalized);
-        return getSegments(normalized);
-      },
-      ciphertext:          function () {
-        return getCipherText(this.plaintextSegments())
-      },
-      normalizeCiphertext: () => getSegments(this.ciphertext).join(' ')
-    };
-  }
-
 }
