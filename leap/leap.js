2db3bc3ad5778d2d71b1fdf956735ccbf7344fde
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-27 08:06:43 -0400

    Revert leap.js

--- leap/leap.js
+++ leap/leap.js
@@ -1,5 +1,9 @@
-export default function(year){
-  if (year === 2016) return true;
-  if (year === 2000) return true;
-  if (year === 1992) return true;
-}
+const mod = (dividend, divisor) => dividend % divisor;
+const is = ({
+  divisibleBy: (a, b) =>  mod(a,b) === 0,
+  notDivisibleBy: (a, b) =>  mod(a,b) !== 0
+});
+
+export default year => is.divisibleBy(year, 400)
+                      || is.divisibleBy(year, 4)
+                      && is.notDivisibleBy(year, 100);
