46ff86150b070320df5b993a1724a9e28f6896a3
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-27 06:15:17 -0400

    Refactor leap TDD

--- leap/leap.js
+++
@@ -1,9 +0,0 @@
-const mod = (dividend, divisor) => dividend % divisor;
-const is = ({
-  divisibleBy: (a, b) =>  mod(a,b) === 0,
-  notDivisibleBy: (a, b) =>  mod(a,b) !== 0
-});
-
-export default year => is.divisibleBy(year, 400)
-                      || is.divisibleBy(year, 4)
-                      && is.notDivisibleBy(year, 100);
